9ce321fdfdcb67ed148613610d1d3164e5767b84
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-02-17 13:03:31 -0500

    MENU_USUARIO

--- script/menu_desplegable.js
+++ script/menu_desplegable.js
@@ -1,104 +1,39 @@
 
 $(document).ready(main);
  
-var contador = 1;
  
 function main(){
 	  contador = 1;
+	  $("#menu_categorias").hide();
 	$("#boton_menu").click(function(){
 		if(contador ==1){
-			$("#menu_categorias").animate({
-				left: '0%'
-			});
-			contador = 0;
-
+		$('#boton_menu').css("color","#18C459");
+		$("#menu_categorias").show();
+         contador=0;
 		} 
-	
-	else { 
+	  else { 
+	  	$('#boton_menu').css("color","#FFFFFF");
+		$("#menu_categorias").hide();
+		contador=1;
+	}
+});
+}
 
-      if($(window).width()<=1100)
-      {
+$(document).ready(function(){
+	$("#menu_usuario_nav").hide();
+	var valor=1;
 
+$("#usuario").on( "click", function() {
+		if(valor ==1){
+			$('#usuario').css("color","#18C459");
+			$("#menu_usuario_nav").show();
+			valor=0;
+		}
+		else
+		{   $('#usuario').css("color","#FFFFFF");
+			$("#menu_usuario_nav").hide();
+			valor=1;
+		}
+});
 
-       if($(window).width()<=1100 && $(window).width()>800)
-
-       {
-
-      $("#menu_categorias").animate({
-				left: '-30%'
-			});
-			contador = 1;	
-      }
-
-      if($(window).width()<=800 && $(window).width()>700)
-      {
-      $("#menu_categorias").animate({
-				left: '-40%'
-			});
-			contador = 1;	
-      }
-
-       if($(window).width()<=700 && $(window).width()>600)
-       {
-      $("#menu_categorias").animate({
-				left: '-45%'
-			});
-			contador = 1;	
-      } 
-
-
-      if($(window).width()<=600 && $(window).width()>500)
-      {
-      $("#menu_categorias").animate({
-				left: '-50%'
-			});
-			contador = 1;
-
-			}	
-
-	  if($(window).width()<=500 && $(window).width()>450)
-      {
-      $("#menu_categorias").animate({
-				left: '-70%'
-			});
-			contador = 1;
-
-			}	
-	
-	if($(window).width()<=400 && $(window).width()>300 )
-      {
-      $("#menu_categorias").animate({
-				left: '-80%'
-			});
-			contador = 1;
-
-			}	
-
-	if($(window).width()<=300 )
-      {
-      $("#menu_categorias").animate({
-				left: '-90%'
-			});
-			contador = 1;
-
-			}	
-      }
-
-      else{
-
-      	$("#menu_categorias").animate({
-				left: '-25%'
-			});
-			contador = 1;
-
-      }
-}		
-		
-
-
-	});
-
-};
-
-
-	
+});
