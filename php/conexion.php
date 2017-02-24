0247db62dae18034ca1a193c0daafe5402997621
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-02-24 12:21:22 -0500

    PHP

---
+++ php/conexion.php
@@ -0,0 +1,59 @@
+<?php
+
+$conexion=null;
+function conexion ()
+{
+global $conexion;
+
+$serverName = "server25.hostinger.es"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"	u807939866_ags", "UID"=>"u807939866_bravo", "PWD"=>"agrosell2017");
+$conexion = mysql_connect( $serverName, $connectionInfo);
+
+if( $conexion ) {
+     /*echo "Conexión establecida.<br />";*/
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( mysql_errors(), true));
+}
+
+return $conexion;
+}
+
+/*
+
+function validarlogin ($idusuario,$contraseña)
+{
+	global $conexion;
+
+
+	$consulta="SELECT * FROM USUARIOS  WHERE  usuario_nvar='$idusuario' AND clave_nvar='$contraseña'";
+	$respuesta=mysql_query($conexion,$consulta);
+	if($fila=mysql_fetch_array($respuesta))
+	{   
+		session_start();
+		$_sesion['usuario_nvar']=$idusuario;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+function desconectar()
+{
+	global $conexion;
+}
+
+
+
+function hainiciarsesion()
+{   
+	session_start();
+	return true;
+}
+
+*/
+
+
+
+?>
