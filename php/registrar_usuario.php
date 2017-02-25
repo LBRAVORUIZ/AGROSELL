1bc877fcb3f8d293c4f2e4c57d2a38c4eb0a41f8
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-02-25 10:46:21 -0500

    PHP7

--- php/registrar_usuario.php
+++ php/registrar_usuario.php
@@ -2,6 +2,7 @@
 require 'conexion.php';
 
 $codigo=$_POST['codigo'];
+
 if ($codigo==0) {
 $nombre=$_POST['nombre_agricultor'];
 $apellido=$_POST['apellidos_agricultor'];
@@ -10,7 +11,7 @@ $clave=$_POST['password_agricultor'];
 $dni=$_POST['dni_agricultor'];
 $telefono=$_POST['telefono_agricultor'];
 $direccion=$_POST['direccion_agricultor'];
-/*
+
 echo $nombre;
 echo $apellido;
 echo $email;
@@ -18,14 +19,14 @@ echo $clave;
 echo $dni;
 echo $telefono;
 echo $direccion;
-
-*/
+/*
 conexion();
 $query1 = "INSERT INTO usuario VALUES(NULL,".$email."',".$clave.")";
 $query2 = "INSERT INTO agricultor VALUES(NULL,".$nombre."',".$apellido.",".$dni.",".$telefono.",".$direccion.")";
 mysqli_query($conexion, $query1);
 mysqli_query($conexion, $query2);
 mysqli_close($conexion);
+*/
 }
 else{
 $razon_social=$_POST['razon_social_comerciante'];
