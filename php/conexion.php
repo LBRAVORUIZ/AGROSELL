9215b8412ec5caf4f8dbae3eabade6b0b59a66ec
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-02-25 10:41:19 -0500

    PHP7

--- php/conexion.php
+++ php/conexion.php
@@ -5,9 +5,9 @@ function conexion ()
 {
 global $conexion;
 
-$conexión = mysqli_connect("mysql.hostinger.es", "u807939866_bravo", "agrosell2017", "u807939866_ags");
+$conexion = mysqli_connect("mysql.hostinger.es", "u807939866_bravo", "agrosell2017", "u807939866_ags");
 
-if (!$conexión) {
+if (!$conexion) {
     echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
     echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
     echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
@@ -15,8 +15,8 @@ if (!$conexión) {
 }
 
 echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
-echo "Información del host: " . mysqli_get_host_info($conexión) . PHP_EOL;
-return $conexión;
+echo "Información del host: " . mysqli_get_host_info($conexion) . PHP_EOL;
+return $conexion;
 }
 ?>
 
