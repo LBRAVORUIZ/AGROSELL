27aa5b845b20c02a606809020562664b166a7667
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-02-26 00:34:47 -0500

    PHP7

--- php/iniciar_sesion.php
+++ php/iniciar_sesion.php
@@ -3,18 +3,19 @@ require 'conexion.php';
 
 $email=$_POST['email'];
 $password=$_POST['password'];
-
-
 conexion();
-if(validarLogin($email,$password)){
+$consulta="SELECT * FROM usuario  WHERE  email_usuario='$email' AND clave_usuario='$password'";
+$respuesta=mysqli_query($conexion,$consulta);
+if($fila=mysqli_fetch_array($respuesta))
+	{   
+		session_start();
+		$_sesion['email_usuario']=$email;
+		header('../index.html');
 
-header('../index.html');
-
-}
-
-else{
-
-?>
+	}
+	else
+	{
+		?>
 <script type="text/javascript">
 	
 alert('Los datos ingresados son incorrectos')
@@ -24,5 +25,6 @@ location.href="../html/iniciar_sesion.html"
 </script>
 
 <?php
-}
+	}
+
  ?>
