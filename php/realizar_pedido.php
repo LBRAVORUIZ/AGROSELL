331300643e2e4cd304fbe1e932b4f733d9dabb36
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-03-06 15:29:17 -0500

    PHP7-PEDIDOS

---
+++ php/realizar_pedido.php
@@ -0,0 +1,15 @@
+<?php 
+
+require 'conexion.php';
+$producto=$_POST['producto'];
+$cantidad=$_POST['cantidad'];
+$calidad=$_POST['calidad'];
+$precio=$_POST['precio'];
+$descripcion=$_POST['descripcion'];
+$direccion=$_POST['direccion'];
+$imagen=addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
+conexion();
+$query = "INSERT INTO pedido VALUES(NULL,'".$producto"','".$cantidad."','".$calidad"','".$precio"','".$descripcion"','".$direccion"','".$imagen"')";
+$mysqli=$conexion->query($query);
+
+ ?>
