ad521b8f6f276d5803dda7865597864b7ffd981c
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-03-06 23:47:22 -0500

    PHP7

--- php/realizar_pedido.php
+++ php/realizar_pedido.php
@@ -1,16 +1,26 @@
 <?php 
 
 require 'conexion.php';
+session_start();
+if (empty($_SESION)) {
+	header("location:../html/iniciar_sesion.html");
+}
+else{
+$usuario=$_SESION['id_usuario'];
 $producto=$_POST['producto'];
 $cantidad=$_POST['cantidad'];
 $calidad=$_POST['calidad'];
 $precio=$_POST['precio'];
 $descripcion=$_POST['descripcion'];
 $direccion=$_POST['direccion'];
+
 $imagen=addslashes(file_get_contents($_FILES['imagen']['tmp_name']));
 conexion();
-$query = "INSERT INTO pedido VALUES(NULL,'".$producto."','".$cantidad."','".$calidad."','".$precio."','".$descripcion."','".$direccion."','".$imagen."')";
+$query = "INSERT INTO pedido VALUES(NULL,'".$usuario."','".$producto."','".$cantidad."','".$calidad."','".$precio."','".$descripcion."','".$direccion."','".$imagen."')";
 $mysqli=$conexion->query($query);
  
 $conexion->close();
+
+header("location:../html/mispedidos.html");
+}
  ?>
