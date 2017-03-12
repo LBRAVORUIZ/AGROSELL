0cdb1e452ea5a73144530a8f5f15897ba7c9c7f0
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-03-12 17:51:48 -0500

    PHP7

--- php/email.php
+++ php/email.php
@@ -6,8 +6,8 @@ conexion();
 $query = "SELECT id_usuario FROM usuario WHERE email_usuario='$email'";
 $resultado = $conexion->query($query);
 $fila = $resultado->fetch_row();
- if (!empty($fila[0])) {
-   echo "ya existe este email";   
+ if ($fila[0]<>"") {
+    echo "ya existe este email";  
  }
  
 $conexion->close();
