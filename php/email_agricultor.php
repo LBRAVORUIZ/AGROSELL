eb63a885dd4a8e443ffdc77f30001d7b0d59fec6
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-03-12 16:39:57 -0500

    PHP7

--- php/email_agricultor.php
+++ php/email_agricultor.php
@@ -6,6 +6,9 @@ conexion();
 $query = "SELECT email_usuario FROM usuario WHERE email_usuario='$email'";
  if ($resultado = $conexion->query($query)) {
       echo "ya existe este email";
+      echo "script>
+      $('#email_agricultor').css({'color':'#C31515'});
+</script>";
  }
 
 $conexion->close();
--- php/email_comerciante.php
+++ php/email_comerciante.php
@@ -5,6 +5,9 @@ conexion();
 $query = "SELECT email_usuario FROM usuario WHERE email_usuario='$email'";
  if ($resultado = $conexion->query($query)) {
       echo "ya existe este email";
+       echo "script>
+      $('#email_comerciante').css({'color':'#C31515'});
+</script>";
  }
 
 $conexion->close();
