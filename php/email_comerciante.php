d596592db5b2c03c3926f41ed86fc4c9d38d24a6
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-03-12 14:44:14 -0500

    PHP7

--- php/email_comerciante.php
+++ php/email_comerciante.php
@@ -1,7 +1,6 @@
 <?php 
 require 'conexion.php';
 $email = $_POST['email'];
-echo $email;
 conexion();
 $query = "SELECT email_usuario FROM usuario WHERE email_usuario='$email'";
  if ($resultado = $conexion->query($query)) {
