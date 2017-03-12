153b77f393b36fd9a482cbe6d9b46211da55254a
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-03-12 16:57:51 -0500

    PHP7

--- php/email_comerciante.php
+++ php/email_comerciante.php
@@ -5,7 +5,7 @@ conexion();
 $query = "SELECT email_usuario FROM usuario WHERE email_usuario='$email'";
  if ($resultado = $conexion->query($query)) {
       echo "ya existe este email";
-       echo "script>
+       echo "<script>
       $('#email_comerciante').css({'color':'#C31515'});
 </script>";
  }
