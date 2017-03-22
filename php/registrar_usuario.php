bad6c16eee6611df216dfe3c0c8b3ae9795f0c64
LBRAVORUIZ <lmbr.bravoruiz@gmail.com>
2017-03-21 19:16:19 -0500

    PHP7

--- php/registrar_usuario.php
+++ php/registrar_usuario.php
@@ -11,15 +11,17 @@ $dni=$_POST['dni_agricultor'];
 $telefono=$_POST['telefono_agricultor'];
 $direccion=$_POST['direccion_agricultor'];
 conexion();
+if(filter_var($email, FILTER_VALIDATE_EMAIL) && filter_var($dni, FILTER_VALIDATE_INT) && filter_var($telefono, FILTER_VALIDATE_INT)){
 $query1 = "INSERT INTO usuario VALUES(NULL,'".$email."','".$clave."')";
 $query2 = "INSERT INTO agricultor VALUES(NULL,'".$email."','".$nombre."','".$apellido."','".$dni."','".$telefono."','".$direccion."')";
 mysqli_query($conexion, $query1);
 mysqli_query($conexion, $query2);
 mysqli_close($conexion);
-header("location:../html/iniciar_sesion.html");
 
+header("location:../html/iniciar_sesion.html");
 }
 else{
+if(filter_var($email, FILTER_VALIDATE_EMAIL) && filter_var($ruc, FILTER_VALIDATE_INT) && filter_var($telefono, FILTER_VALIDATE_INT)){
 $razon_social=$_POST['razon_social_comerciante'];
 $ruc=$_POST['ruc_comerciante'];
 $email=$_POST['email'];
@@ -33,6 +35,7 @@ $query2 = "INSERT INTO empresa VALUES(NULL,'".$email."','".$razon_social."','".$
 mysqli_query($conexion, $query1);
 mysqli_query($conexion, $query2);
 mysqli_close($conexion);
+}
 header("location:../html/iniciar_sesion.html");
 
 }
